4d119b941184c30f1b75f9e88c0db78bcf8e41a0
Quentin RUHF <quentin.ruhf@gmail.com>
2025-04-02 10:44:36 +0200

    fix zombie

--- NTS-Proj/Assets/Scripts/Zombie.cs
+++ NTS-Proj/Assets/Scripts/Zombie.cs
@@ -18,19 +18,4 @@ public class Zombie : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, cam.transform.position, Time.deltaTime * speed);
         transform.LookAt(cam.transform);
     }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "MainCamera")
-        {
-            Destroy(gameObject);
-        }
-    }
-    
-    private void OnDestroy()
-    {
-        // Instantiate a particle effect and destroy it after 2 seconds
-        var particle = Instantiate(particlePrefab, transform.position, Quaternion.identity);
-        Destroy(particle, 2f);
-    }
 }
